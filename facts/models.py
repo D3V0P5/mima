10bb84c5966e77d408b4ae0f05a487667d16813e
D3V0P5 <lala@lala.land>
2016-06-26 17:52:22 +0300

    db issues

--- facts/models.py
+++ facts/models.py
@@ -1,18 +1,18 @@
 from django.db import models
-
+from datetime import datetime
 
 # Create your models here.
 
 
 class Artist(models.Model):
     name = models.CharField(max_length=30, db_index=True , unique=True)
-    last_name = models.CharField(max_length=30, default="")
+   # last_name = models.CharField(max_length=30, default="")
 
 
 class Song(models.Model):
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name="songs")
     name = models.CharField(max_length=30, db_index=True)
-    release_date = models.DateField(db_index=True)
+    #release_date = models.DateField(db_index=True , default=datetime.now)
     # num_stars = models.IntegerField()
 
 
