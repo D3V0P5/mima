50fa6739cd0d4ddcfe62af621ffb0a8cf3a8c593
server <server>
2016-07-06 15:54:54 +0000

    uwsgi is a b%%#

--- Mima/wsgi.py
+++ Mima/wsgi.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 *-*
 """
 WSGI config for Mima project.
 
@@ -6,11 +7,17 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
-
-import os
-
+import os , sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Mima.settings")
+#os.environ['LC_ALL']="it_IT.UTF-8"
+#locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+#sys.setdefaultencoding('utf-8')
+#import sys
+reload(sys)  # Reload does the trick!
+sys.setdefaultencoding('UTF8')
+
 
 application = get_wsgi_application()
+
